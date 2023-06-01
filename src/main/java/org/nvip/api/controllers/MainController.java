6b60a8baae1c23d18a0721745e6cc194dc94b122
Chris Enoch <ctevse@rit.edu>
2023-06-01 22:09:13 +0000

    Completed User login controller refactor

--- src/main/java/org/nvip/api/controllers/MainController.java
+++ src/main/java/org/nvip/api/controllers/MainController.java
@@ -1,15 +1,24 @@
 package org.nvip.api.controllers;
 
+import org.nvip.data.repositories.MainRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class MainController {
 
+    final MainRepository mainRepository;
+
+    public MainController(MainRepository mainRepository) {
+        this.mainRepository = mainRepository;
+    }
+
     @GetMapping
-    public String getVulnerabilitySummaries(){
-        return "Hello";
+    public Map<String, String> getVulnerabilitySummaries(){
+        return mainRepository.getMainPageCounts();
     }
 }
