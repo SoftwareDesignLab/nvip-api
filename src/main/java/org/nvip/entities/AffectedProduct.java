b2719550ce6ea3a3f702b1df203dd64a503e66f9
Andromeda Sawtelle <zts7211@rit.edu>
2023-07-18 14:02:09 -0400

    Added new description entities and cleaned up some imports

--- src/main/java/org/nvip/entities/AffectedProduct.java
+++ src/main/java/org/nvip/entities/AffectedProduct.java
@@ -5,13 +5,15 @@ import lombok.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
 @Entity
 @Getter
 @Setter
 @JsonIgnoreProperties(value = "vulnerability")
-public class AffectedRelease {
+public class AffectedProduct {
     @Id @Column(name="affected_product_id") private int id;
 
     @ManyToOne @JoinColumn(name="cve_id", referencedColumnName = "cveId")
