df5e3f7ba6bc95821bcfca07639dd6f76f7493ee
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2023-07-27 10:39:22 -0400

    api update WIP

--- src/main/java/org/nvip/entities/AffectedProduct.java
+++ src/main/java/org/nvip/entities/AffectedProduct.java
@@ -10,32 +10,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Entity
-@Table(name="affectedproduct")
+@Table(name="affectedproduct", schema = "nvip")
 @Getter
 @Setter
 @JsonIgnoreProperties(value = "vulnerability")
 public class AffectedProduct {
-    @NonNull @Id @Column(name="affected_product_id")
+    @Id
     private int affectedProductId;
 
     @NonNull @ManyToOne @JoinColumn(name="cve_id", referencedColumnName = "cveId")
     private Vulnerability vulnerability;
 
-    @Column(name="cpe", columnDefinition="VARCHAR(300)", nullable=false)
+    @Basic private LocalDateTime releaseDate;
+
     private String cpe;
 
-    @Basic @Column(name="release_date", nullable=false) 
-    private LocalDateTime releaseDate;
-
-    @Column(name="product_name", columnDefinition="tinytext", nullable=false)
     private String productName;
 
-    @Column(name="version", columnDefinition="tinytext", nullable=false)
     private String version;
 
-    @Column(name="vendor", columnDefinition="tinytext", nullable=false)
     private String vendor;
 
-    @Column(name="purl", columnDefinition="tinytext", nullable=false)
     private String purl;
+
+    private String swidTag;
 }
