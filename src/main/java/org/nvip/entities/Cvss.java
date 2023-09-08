f7dedfaaca2262320db6ede38a0505a13dab4ae7
Chris Enoch <ctevse@rit.edu>
2023-09-08 15:14:04 +0000

    Changed impactScore to transient since it is no longer stored in the db

--- src/main/java/org/nvip/entities/Cvss.java
+++ src/main/java/org/nvip/entities/Cvss.java
@@ -45,6 +45,7 @@ public class Cvss {
     @ManyToOne @JoinColumn(name="cve_id", referencedColumnName = "cveId")
     Vulnerability vulnerability;
 	private double baseScore;
+	@Transient private double impactScore;
     @Basic private LocalDateTime createDate;
     private Integer userId;
 
