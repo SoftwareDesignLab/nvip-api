5883683fa6322db6002e4d40b2770e3515b60758
Corey Urbanke <cju8676@rit.edu>
2023-10-11 13:22:13 -0400

    updated MainController to lombok

--- src/main/java/org/nvip/api/controllers/MainController.java
+++ src/main/java/org/nvip/api/controllers/MainController.java
@@ -1,23 +1,19 @@
 package org.nvip.api.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.nvip.data.repositories.MainRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequiredArgsConstructor
 public class MainController {
 
     final MainRepository mainRepository;
 
-    public MainController(MainRepository mainRepository) {
-        this.mainRepository = mainRepository;
-    }
-
-    @GetMapping
+    @GetMapping("/")
     public Map<String, String> getVulnerabilitySummaries(){
         return mainRepository.getMainPageCounts();
     }
