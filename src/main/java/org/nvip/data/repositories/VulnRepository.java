978b369fc7d4c21a391037afd8eaaf7493a831ff
Corey Urbanke <cju8676@rit.edu>
2023-10-18 12:34:58 -0400

    combined vulnerability serializers

--- src/main/java/org/nvip/data/repositories/VulnRepository.java
+++ src/main/java/org/nvip/data/repositories/VulnRepository.java
@@ -40,28 +40,4 @@ public interface VulnRepository extends JpaRepository<Vulnerability, Long> {
             @Param("limitCount") Integer limitCount,
             @Param("product") String product
     );
-//    @Query("SELECT DISTINCT v " +
-//            "FROM Vulnerability v " +
-//            "LEFT JOIN v.exploits e " +
-//            "LEFT JOIN v.affectedProducts ap " +
-//            "LEFT JOIN v.vdoCharacteristics vc " +
-//            "LEFT JOIN v.cvssScore c " +
-//            "WHERE (:keyword IS NULL OR v.description.description LIKE %:keyword%) " +
-//            "AND (:startDate IS NULL OR v.createdDate >= :startDate) " +
-//            "AND (:endDate IS NULL OR v.createdDate <= :endDate) " +
-//            "AND ((:cvssScores) IS NULL OR c.baseScore IN (:cvssScores)) " +
-//            "AND (:product IS NULL OR ap.cpe LIKE %:product%) " +
-//            "AND ((:vdoLabels) IS NULL OR vc.vdoLabel IN (:vdoLabels)) " +
-//            "GROUP BY v.vulnId " +
-//            "ORDER BY v.vulnId DESC " +
-//            "LIMIT :limitCount")
-//    List<Vulnerability> searchVulnerabilities(
-//            @Param("keyword") String keyword,
-//            @Param("startDate") LocalDateTime startDate,
-//            @Param("endDate") LocalDateTime endDate,
-//            @Param("cvssScores") double[] cvssScores,
-//            @Param("vdoLabels") String[] vdoLabels,
-//            @Param("limitCount") Integer limitCount,
-//            @Param("product") String product
-//    );
 }
