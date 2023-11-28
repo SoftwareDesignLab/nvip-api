4ed53a6f2c13370d6bd5116f7cfb180de26b062e
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2023-11-28 17:13:52 -0500

    Fixed VdoSet/VdoCharacteristics logic

--- src/main/java/org/nvip/entities/VdoSet.java
+++ src/main/java/org/nvip/entities/VdoSet.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,6 +16,11 @@ public class VdoSet {
     @Id private int vdoSetId;
 
     @Basic private LocalDateTime createdDate;
+
+    @ToString.Exclude
+    @OneToMany
+    @JoinColumn(name="vdo_set_id", referencedColumnName = "vdoSetId")
+    List<VdoCharacteristic> vdoCharacteristics;
 
     private double cvssBaseScore;
 
