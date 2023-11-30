d61341c64ccea676f076d99e9415ef4b5498ca52
Mackenzie Wade <you@example.com>
2023-11-30 13:40:47 -0500

    tests for the serializer branch are completed

--- src/test/java/org/nvip/api/serializers/UserDTOTest.java
+++ src/test/java/org/nvip/api/serializers/UserDTOTest.java
@@ -16,7 +16,7 @@ public class UserDTOTest {
     public void testGettersAndSetters() {
         UserDTO userDTO = new  UserDTO(0,"old token","old userName", 0,oldtime);
         userDTO.setUserID(1);
-        userDTO.setToken("new token");
+        userDTO.setToken("new tokenz");
         userDTO.setUserName("new UserName");
         userDTO.setExpirationDate(newTime);
         userDTO.setRoleId(1);
@@ -24,7 +24,7 @@ public class UserDTOTest {
 
 
         assertEquals(1,  userDTO.getUserID());
-        assertEquals("new token",     userDTO.getToken());
+        assertEquals("new tokenz",     userDTO.getToken());
         assertEquals("new UserName" ,  userDTO.getUserName());
         assertEquals(newTime, userDTO.getExpirationDate());
         assertEquals(1, userDTO.getRoleId());
