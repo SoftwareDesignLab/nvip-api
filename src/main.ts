c4844ddbfe5ef6cb496049af683282bba01d2a6d
ibrahimvse <ijmujhid@hawaii.edu>
2024-03-29 00:47:45 -1000

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://localhost:4200','https://www.cve.live/'], 
+        origin: ['http://localhost:4200','https://www.cve.live','https://cve.live'], 
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         allowedHeaders: 'Content-Type, Accept',
     })
